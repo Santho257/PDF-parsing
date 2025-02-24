1870e159aea2e69323d6b3261e6796b5cd578df5
Santho257 <santhoshkumar02553@gmail.com>
2025-02-24 19:29:28 +0530

    Extracxting Highlighted Text Data

--- src/main/java/com/santho/Main.java
+++ src/main/java/com/santho/Main.java
@@ -3,12 +3,14 @@ package com.santho;
 import com.santho.pdfparser.PdfParser;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
 //        PdfParser.createTextPDF();
-        File file = new File("/Users/santhosh-22951/Documents/Zoho-Java-Learnings/HyperLinkParsing/src/main/resources/learning.pdf");
+        File file = new File("/Users/santhosh-22951/Documents/Zoho-Java-Learnings/HyperLinkParsing/src/main/resources/Sample.pdf");
+        System.out.println(PdfParser.highlightedTexts(file).toString().replace("\n", "").replace("], ", "]\n"));
+        System.out.println(PdfParser.highlightedTexts(file, new float[]{1, 1, 0.6f}).toString());
 //        System.out.println(PdfParser.links(file));
-        System.out.println(PdfParser.textContents(file));
     }
 }
--- src/main/java/com/santho/pdfparser/PdfParser.java
+++ src/main/java/com/santho/pdfparser/PdfParser.java
@@ -7,14 +7,14 @@ import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.PDPageTree;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.apache.pdfbox.pdmodel.interactive.action.PDActionURI;
-import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotation;
-import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotationLink;
+import org.apache.pdfbox.pdmodel.interactive.annotation.*;
 import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.text.PDFTextStripperByArea;
 
+import java.awt.geom.Rectangle2D;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static org.apache.pdfbox.pdmodel.font.Standard14Fonts.FontName.TIMES_ROMAN;
 
@@ -52,17 +52,103 @@ public class PdfParser {
         }
     }
 
+    public static Map<Integer, List<String>> highlightedTexts(File pdfFile, float[] color){
+        if (isNotPdf(pdfFile)) throw new IllegalArgumentException("Not a PDF file");
+        Map<Integer, List<String>> highlightedTexts= new HashMap<>();
+        try (PDDocument document = Loader.loadPDF(pdfFile)) {
+            PDPageTree allPages = document.getPages();
+            int pageNo = 1;
+            for (PDPage page : allPages) {
+                List<PDAnnotation> annotations = page.getAnnotations();
+                PDFTextStripperByArea areaStripper = new PDFTextStripperByArea();
+                for (PDAnnotation annotation : annotations) {
+                    if (annotation.getSubtype().equalsIgnoreCase("highlight")) {
+                        PDAnnotationHighlight highlight = (PDAnnotationHighlight) annotation;
+                        if(compare(annotation.getColor().getComponents(), color)){
+                            Rectangle2D[] activeArea = getActiveRectangle(highlight.getQuadPoints(), page.getCropBox().getHeight());
+                            for (Rectangle2D rect : activeArea)
+                                areaStripper.addRegion("hightlight-" + rect.hashCode(), rect);
+                        }
+                    }
+                }
+                areaStripper.extractRegions(page);
+                areaStripper.setSortByPosition(true);
+                List<String> highlighted = areaStripper.getRegions();
+                List<String> marked = new ArrayList<>();
+                for (String region : highlighted) {
+                    marked.add(areaStripper.getTextForRegion(region));
+                }
+                if(!marked.isEmpty())
+                    highlightedTexts.put(pageNo, marked);
+                pageNo++;
+            }
+            return highlightedTexts;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            throw new IllegalArgumentException("Error fetching links");
+        }
+    }
+
+    private static boolean compare(float[] components, float[] color) {
+        if(components.length != color.length){
+            return false;
+        }
+        for(int i = 0; i < components.length; i++){
+            if(Math.abs(components[i] - color[i]) >= 0.001){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static Map<Integer, List<String>> highlightedTexts(File pdfFile) {
+        if (isNotPdf(pdfFile)) throw new IllegalArgumentException("Not a PDF file");
+        Map<Integer, List<String>> highlightedTexts= new HashMap<>();
+        try (PDDocument document = Loader.loadPDF(pdfFile)) {
+            PDPageTree allPages = document.getPages();
+            int pageNo = 1;
+            for (PDPage page : allPages) {
+                List<PDAnnotation> annotations = page.getAnnotations();
+                PDFTextStripperByArea areaStripper = new PDFTextStripperByArea();
+                for (PDAnnotation annotation : annotations) {
+                    if (annotation.getSubtype().equalsIgnoreCase("highlight")) {
+                        System.out.println(annotation.getColor());
+                        PDAnnotationHighlight highlight = (PDAnnotationHighlight) annotation;
+                        Rectangle2D[] activeArea = getActiveRectangle(highlight.getQuadPoints(), page.getCropBox().getHeight());
+                        for (Rectangle2D rect : activeArea)
+                            areaStripper.addRegion("hightlight-" + rect.hashCode(), rect);
+                    }
+                }
+                areaStripper.extractRegions(page);
+                areaStripper.setSortByPosition(true);
+                List<String> highlighted = areaStripper.getRegions();
+                List<String> marked = new ArrayList<>();
+                for (String region : highlighted) {
+                    marked.add(areaStripper.getTextForRegion(region));
+                }
+                if(!marked.isEmpty())
+                    highlightedTexts.put(pageNo, marked);
+                pageNo++;
+            }
+            return highlightedTexts;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            throw new IllegalArgumentException("Error fetching links");
+        }
+    }
+
     public static List<String> links(File pdfFile) {
         if (isNotPdf(pdfFile)) throw new IllegalArgumentException("Not a PDF file");
         List<String> urls = new ArrayList<>();
         try (PDDocument document = Loader.loadPDF(pdfFile)) {
-            PDPageTree allPages =  document.getPages();
-            for(PDPage page : allPages){
+            PDPageTree allPages = document.getPages();
+            for (PDPage page : allPages) {
                 List<PDAnnotation> annotations = page.getAnnotations();
-                for(PDAnnotation annotation : annotations){
-                    PDAnnotationLink link = (PDAnnotationLink) annotation;
-                    if(link.getAction() instanceof PDActionURI){
-                        urls.add(((PDActionURI) link.getAction()).getURI());
+                for (PDAnnotation annotation : annotations) {
+                    if (annotation instanceof PDAnnotationLink) {
+                        PDAnnotationLink link = (PDAnnotationLink) annotation;
+                        if (link.getAction() instanceof PDActionURI)
+                            urls.add(((PDActionURI) link.getAction()).getURI());
                     }
                 }
             }
@@ -76,5 +162,22 @@ public class PdfParser {
     private static boolean isNotPdf(File file) {
         String filename = file.getName();
         return !"pdf".equalsIgnoreCase(filename.substring(filename.lastIndexOf(".") + 1));
+    }
+
+    private static Rectangle2D[] getActiveRectangle(float[] quadPoints, float pageHeight) {
+        if (quadPoints == null || quadPoints.length < 8) {
+            return new Rectangle2D[0];
+        }
+        Rectangle2D[] activeRectangles = new Rectangle2D[quadPoints.length / 8];
+        for (int i = 0, j = 0; i < quadPoints.length; i += 8, j++) {
+            float x1 = i + quadPoints[0];
+            float y1 = i + quadPoints[1];
+            float x2 = i + quadPoints[2];
+            float y3 = i + quadPoints[5];
+            float width = x2 - x1;
+            float height = y1 - y3;
+            activeRectangles[j] = new Rectangle2D.Float(x1, pageHeight - y1, width, height);
+        }
+        return activeRectangles;
     }
 }
